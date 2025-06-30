e88144943bc8d1977ff1d9a93871a4e9807913ec
Mike <mclancy96@gmail.com>
2025-06-30 10:44:47 -0400

    Confirm mui integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Container from "@mui/material/Container";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container
+        maxWidth="sm"
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <h1>Welcome to Power Music</h1>
+
+      </Container>
     </div>
   );
 }
