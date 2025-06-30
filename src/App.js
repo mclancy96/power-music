17333827c818efb37b72d8278a2c919f33a4e12b
Mike <mclancy96@gmail.com>
2025-06-30 13:04:02 -0400

    Add router (in comments for when I have internet)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import "./App.css";
 import Container from "@mui/material/Container";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navbar from "./Navbar";
+
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Navbar />
+//         <Container>
+//           <Switch>
+//             {/* <Route path="/about">
+//               <About />
+//             </Route>
+//             <Route path="/users">
+//               <Users />
+//             </Route> */}
+//             <Route path="/">
+//               <Home />
+//             </Route>
+//           </Switch>
+//         </Container>
+//       </div>
+//     </Router>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <Container
-        maxWidth="sm"
-        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-      >
-        <h1>Welcome to Power Music</h1>
-
-      </Container>
-    </div>
+    <Container>
+      <h1>Welcome to Power Music</h1>
+    </Container>
   );
 }
+
+//https://api.jamendo.com/v3.0/radios/?client_id=your_client_id&format=jsonpretty&limit=3
 
 export default App;
