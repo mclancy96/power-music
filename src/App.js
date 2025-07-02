d5096ebbc75667517443d058db7ce54fafcc5f2d
Mike <mclancy96@gmail.com>
2025-07-02 13:07:02 -0400

    fix app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Navbar from "./Navbar";
 import Search from "./Search";
 import Home from "./Home";
-import { useState } from "react";
+import TrackDetails from "./TrackDetails";
+import { useState, useEffect } from "react";
+import Favorites from "./Favorites";
 
 function App() {
   const [player, setPlayer] = useState({
@@ -11,9 +13,96 @@ function App() {
     audio: {},
     isPlaying: false,
   });
+  const [shouldPlay, setShouldPlay] = useState(false);
+  const favoriteUrl = process.env.REACT_APP_DB_URL + "favorites/";
+  const [favorites, setFavorites] = useState([]);
+  const isTrackInFavorites = (track) => {
+    return favorites.find(
+      (fave) => fave.track_id === track.id || fave.id === track.id,
+    );
+  };
+  useEffect(() => {
+    fetch(favoriteUrl)
+      .then((r) => r.json())
+      .then(setFavorites);
+  }, []);
+  const onFavoriteButtonClick = (track) => {
+    const foundTrackFavorite = isTrackInFavorites(track);
+    foundTrackFavorite
+      ? deleteTrackFromFavorites(foundTrackFavorite)
+      : addTrackToFavorites(track);
+  };
+
+  const deleteTrackFromFavorites = (favorite) => {
+    fetch(favoriteUrl + favorite.id, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => {
+        setFavorites((faves) =>
+          faves.filter(
+            (fave) => fave.track_id !== favorite.id && fave.id !== favorite.id,
+          ),
+        );
+      })
+      .catch(() => {
+        return false;
+      });
+  };
+
+  const formatFavoriteBody = ({
+    id,
+    name,
+    duration,
+    artist_name,
+    album_name,
+    releasedate,
+    album_image,
+    audio,
+    shareurl,
+    image,
+  }) => ({
+    track_id: id,
+    name,
+    duration,
+    artist_name,
+    album_name,
+    releasedate,
+    album_image,
+    audio,
+    shareurl,
+    image,
+  });
+
+  const addTrackToFavorites = (track) => {
+    fetch(favoriteUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formatFavoriteBody(track)),
+    })
+      .then((r) => r.json())
+      .then((returnedTrack) => {
+        setFavorites((faves) => [...faves, returnedTrack]);
+      })
+      .catch(() => {
+        return false;
+      });
+  };
+
+  useEffect(() => {
+    if (shouldPlay && player.track.audio) {
+      playSong();
+      setShouldPlay(false);
+    }
+  }, [player.track, shouldPlay]);
+
   const playSong = () => {
     if (player.track.audio) {
-      const audio = new Audio(player.track.audio);
+      let audio = player.audio;
+      if (!audio.paused) {
+        audio = new Audio(player.track.audio);
+      }
+
       audio.play().catch((error) => {
         console.error("Error playing audio:", error);
       });
@@ -22,10 +111,11 @@ function App() {
       console.log("No preview available for this track");
     }
   };
+
   const pauseSong = () => {
     if (player.audio && typeof player.audio.pause === "function") {
       player.audio.pause();
-      setPlayer((player) => ({ ...player, isPlaying: false, audio: {} }));
+      setPlayer((player) => ({ ...player, isPlaying: false }));
     } else {
       console.log("No preview available for this track");
     }
@@ -34,16 +124,11 @@ function App() {
     if (player.isPlaying) pauseSong();
     else playSong();
   };
-  const queueTrack = (track) => {
-    if (player.isPlaying) pauseSong();
 
-    return new Promise((resolve) => {
-      setPlayer((prevPlayer) => {
-        const newPlayerState = { ...prevPlayer, track };
-        setTimeout(() => resolve(newPlayerState), 0);
-        return newPlayerState;
-      });
-    });
+  const queueTrackAndPlay = (track) => {
+    if (player.isPlaying) pauseSong();
+    setPlayer((prevPlayer) => ({ ...prevPlayer, track, audio: {} }));
+    setShouldPlay(true);
   };
   return (
     <Router>
@@ -52,17 +137,63 @@ function App() {
         <Container>
           <Routes>
             <Route
-              path="/search"
-              element={<Search {...{ player, togglePlayer, queueTrack }} />}
+              path="/tracks/:trackId"
+              element={
+                <TrackDetails
+                  {...{ player, togglePlayer, queueTrackAndPlay }}
+                />
+              }
             />
-            <Route path="/" element={<Home />} />
+            <Route
+              path="/favorites"
+              element={
+                <Favorites
+                  {...{
+                    player,
+                    togglePlayer,
+                    queueTrackAndPlay,
+                    favorites,
+                    onFavoriteButtonClick,
+                    isTrackInFavorites,
+                  }}
+                />
+              }
+            />
+            <Route
+              path="/search"
+              element={
+                <Search
+                  {...{
+                    player,
+                    togglePlayer,
+                    queueTrackAndPlay,
+                    favorites,
+                    onFavoriteButtonClick,
+                    isTrackInFavorites,
+                  }}
+                />
+              }
+            />
+            <Route
+              path="/"
+              element={
+                <Home
+                  {...{
+                    player,
+                    togglePlayer,
+                    queueTrackAndPlay,
+                    favorites,
+                    onFavoriteButtonClick,
+                    isTrackInFavorites,
+                  }}
+                />
+              }
+            />
           </Routes>
         </Container>
       </div>
     </Router>
   );
 }
-
-//https://api.jamendo.com/v3.0/radios/?client_id=your_client_id&format=jsonpretty&limit=3
 
 export default App;
